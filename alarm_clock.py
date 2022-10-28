ee46d4fef20a08b0bca13d3f6f89ccffbd4c0e06
Kashar Grays <kash.different@Kashars-MacBook-Pro.local>
2022-10-28 16:18:10 -0400

    Alarm clock class is functional

--- alarm_clock.py
+++ alarm_clock.py
@@ -5,8 +5,8 @@ class AlarmClock:
     def __init__(self):
         self.time = []
         self.alarm =  []
-        self.a_time = ''
-    
+        self.a_time = ""
+
     def set_time(self):
         self.time = int(input("What time is it?\n"))
         print("The time is :", self.time)
@@ -16,8 +16,8 @@ class AlarmClock:
         print("Your alarm is set for", self.alarm)
     
     def alarm_set(self):
-        self.a_time = input("Is your on or off?")
-        if self.a_time == "on":
-            print("Alarm is on")
-        elif self.a_time == "off":
-            print("Alarm is off")
+        self.a_time = input("Do you want to set your alarm? Y / N\n")
+        if self.a_time == "Y":
+            print("Alarm is set")
+        elif self.a_time == "N":
+            print("Alarm is not set")
--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from alarm_clock import AlarmClock
 
+# Print the clock’s time to the terminal
+# Call the alarm clock’s change time method to change the time
+# Toggle the alarm clock’s on off switch
+
+
 alarm_1 = AlarmClock()
 alarm_1.set_time()
 alarm_1.set_alarm()
